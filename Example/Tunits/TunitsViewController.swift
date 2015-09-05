24d3f647ca313902540b54f7718bd58c8be2d4b7
Thomas Leu <tomleu@gmail.com>
2015-09-05 14:04:28 -0500

    Connect storyboard and view controller

--- Example/Tunits/TunitsViewController.swift
+++ Example/Tunits/TunitsViewController.swift
@@ -6,8 +6,34 @@
 //  Copyright (c) 2015 CocoaPods. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class TunitsViewController: UIViewController {
+class TunitsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+// MARK: - Properties
+    
+    @IBOutlet weak var tableView : UITableView?
+    
+    weak var datePicker : UIDatePicker?
+    
+// MARK: - Table View
 
+    let kNumberOfSections = 4
+    let kDatePickerSection = 0
+    let kMonthsInYearSection = 1
+    let kDaysInMonthSection = 2
+    let kHoursInDaySection = 3
+    let kMinutesInHourSection = 4
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
