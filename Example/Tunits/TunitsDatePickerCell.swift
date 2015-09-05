008284cf7ffce8f7f781bd3b5354a7e1f549e4a3
Thomas Leu <tomleu@gmail.com>
2015-09-05 15:13:29 -0500

    Create simple table view example app

---
+++ Example/Tunits/TunitsDatePickerCell.swift
@@ -0,0 +1,13 @@
+//
+//  TunitsDatePickerCell.swift
+//  Tunits
+//
+//  Created by Tom on 9/5/15.
+//  Copyright (c) 2015 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class TunitsDatePickerCell: UITableViewCell {
+    @IBOutlet weak var datePicker : UIDatePicker?
+}
--- Example/Tunits/TunitsViewController.swift
+++ Example/Tunits/TunitsViewController.swift
@@ -7,33 +7,133 @@
 //
 
 import UIKit
+import Tunits
 
 class TunitsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-// MARK: - Properties
+// MARK: - Properties and Lifecycle
     
     @IBOutlet weak var tableView : UITableView?
-    
     weak var datePicker : UIDatePicker?
+    
+    lazy var dateFormatter : NSDateFormatter = {
+        var tempFormatter = NSDateFormatter()
+        tempFormatter.dateFormat = NSDateFormatter.dateFormatFromTemplate("MMMM dd, yyyy j:mm", options: 0, locale: NSLocale.autoupdatingCurrentLocale())
+        return tempFormatter
+    }()
+    
+    var timeUnits : [[NSDate]]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView!.delegate = self
+        self.tableView!.dataSource = self
+        self.tableView!.setTranslatesAutoresizingMaskIntoConstraints(false)
+    }
+    
+// MARK: - UI Events
+    
+    func dateChanged(sender:UIDatePicker) {
+        let tunits = TimeUnit()
+        
+        self.timeUnits = [
+            tunits.monthsOfYear(sender.date),
+            tunits.daysOfMonth(sender.date),
+            tunits.hoursOfDay(sender.date),
+            tunits.minutesOfHour(sender.date),
+        ]
+        
+        self.tableView!.reloadData()
+    }
     
 // MARK: - Table View
 
-    let kNumberOfSections = 4
-    let kDatePickerSection = 0
-    let kMonthsInYearSection = 1
-    let kDaysInMonthSection = 2
-    let kHoursInDaySection = 3
-    let kMinutesInHourSection = 4
+    let kNumberOfSections =         5
+    let kDatePickerSection =        0
+    let kMonthsOfYearSection =      1
+    let kDaysOfMonthSection =       2
+    let kHoursOfDaySection =        3
+    let kMinutesOfHourSection =     4
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
+    let kDatePickerCellHeight : CGFloat = 216.0
+    let kTimeUnitCellHeight : CGFloat = 44.0
+    
+    let kDatePickerCellIdentifier = "datePickerCell"
+    let kTimeUnitCellIdentifier =   "timeUnitCell"
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 0
+        return kNumberOfSections
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if indexPath.section == kDatePickerSection {
+            return kDatePickerCellHeight
+        } else {
+            return kTimeUnitCellHeight
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == kDatePickerSection {
+            return 1
+        } else {
+            if let timeUnits = self.timeUnits {
+                return timeUnits[section - 1].count
+            } else {
+                return 0
+            }
+        }
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+            case kMonthsOfYearSection:
+                return "Months Of Year"
+            case kDaysOfMonthSection:
+                return "Days Of Month"
+            case kHoursOfDaySection:
+                return "Hours Of Day"
+            case kMinutesOfHourSection:
+                return "Minutes Of Hour"
+            default:
+                return nil
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        if indexPath.section == kDatePickerSection {
+            return self.datePickerCell()
+        } else {
+            return self.timeUnitCell(indexPath)
+        }
+    }
+    
+    private func datePickerCell() -> UITableViewCell {
+        let datePickerCell : TunitsDatePickerCell = self.tableView!.dequeueReusableCellWithIdentifier(kDatePickerCellIdentifier) as! TunitsDatePickerCell
+        
+        if self.datePicker == nil {
+            self.datePicker = datePickerCell.datePicker!
+            self.datePicker!.addTarget(self, action: "dateChanged:", forControlEvents: .ValueChanged)
+            self.datePicker!.date = NSDate()
+        }
+        
+        return datePickerCell
+    }
+    
+    private func timeUnitCell(indexPath:NSIndexPath) -> UITableViewCell {
+        let timeUnitCell = self.tableView!.dequeueReusableCellWithIdentifier(kTimeUnitCellIdentifier) as! UITableViewCell
+        
+        timeUnitCell.textLabel!.text = self.titleForTimeUnitCell(indexPath)
+        
+        return timeUnitCell
+    }
+    
+    private func titleForTimeUnitCell(indexPath:NSIndexPath) -> String {
+        if let timeUnits = self.timeUnits {
+            return self.dateFormatter.stringFromDate(timeUnits[indexPath.section - 1][indexPath.row])
+        } else {
+            return ""
+        }
     }
 }
