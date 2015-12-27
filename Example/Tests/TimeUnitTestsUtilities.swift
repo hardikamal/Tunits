e7e2b3e16d80e93249b5a37532fa0195a861e9b2
Thomas Leu <tomleu@gmail.com>
2015-12-27 16:39:24 -0600

    Add utility class for test calendars

--- Example/Tests/TimeUnitTestsUtilities.swift
+++ Example/Tests/TimeUnitTestsUtilities.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+import Tunits
+
+public class TimeUnitTestsUtilities : NSObject {
+    public enum Weekday : Int {
+        case Sunday = 1
+        case Monday = 2
+        case Tuesday = 3
+        case Wednesday = 4
+        case Thursday = 5
+        case Friday = 6
+        case Saturday = 7
+    }
+    
+    public func timeUnitWithCalendarWithMondayAsFirstWeekday() -> TimeUnit {
+        return self.timeUnitWithCalendarWithFirstWeekday(Weekday.Monday);
+    }
+    
+    public func timeUnitWithCalendarWithSundayAsFirstWeekday() -> TimeUnit {
+        return self.timeUnitWithCalendarWithFirstWeekday(Weekday.Sunday);
+    }
+    
+    public func timeUnitWithCalendarWithFirstWeekday(weekday : Weekday) -> TimeUnit {
+        let tunit = TimeUnit()
+        tunit.calendar = self.calendarWithFirstWeekday(weekday)
+        return tunit
+    }
+    
+    public func calendarWithSundayAsFirstWeekday() -> NSCalendar {
+        return self.calendarWithFirstWeekday(Weekday.Sunday)
+    }
+    
+    public func calendarWithMondayAsFirstWeekday() -> NSCalendar {
+        return self.calendarWithFirstWeekday(Weekday.Monday)
+    }
+    
+    public func calendarWithFirstWeekday(weekday: Weekday) -> NSCalendar {
+        let calendar = NSCalendar.currentCalendar()
+        calendar.firstWeekday = weekday.rawValue
+        return calendar
+    }
+}
